e356d2468f3b61a8744e67787d58e25869c5ba36
harryahlas <42125399+harryahlas@users.noreply.github.com>
2020-03-26 22:44:40 -0700

    Upload v2

--- what_if_app_v2.R
+++ what_if_app_v2.R
@@ -7,20 +7,33 @@ library(shiny)
 library(DT)
 
 
-# Main data
-df <- read_csv("process_name,true_positives,false_negatives,false_positives,true_negatives
-    1. Current State,10000,0,90000,0
-    2. Ideal State,10000,0,0,90000
-    3. Wide Net Proposal,9900,100,86150,3850
-    4. Medium Net Proposal,7500,2500,40000,50000
-    5. Narrow Net Proposal,2000,8000,2500,87500
-    6. Goal,8000,2000,30000,60000") 
-
 ui <- fluidPage(
   
   titlePanel("What if scenarios"),
   sidebarLayout(
-    sidebarPanel(width = 3,
+    sidebarPanel(width = 4,
+      
+      # Input: text input for the number of texts to be reviewed ----
+      textInput(inputId = "commentCount",
+                label = "Number of Comments:",
+                value = 180000),  
+      
+      # Input: text input for the number of texts that are positives ----
+      sliderInput(inputId = "issueRate",
+                label = "Number of Comments that should be Referred:",
+                value = .015,
+                min = 0,
+                max = .25), 
+      
+      # Input: text input for the cost of each manual review ----
+      textInput(inputId = "precision",
+                label = "Precision (% of predicted positives that are correct):",
+                value = .013),
+
+      # Input: text input for the cost of each manual review ----
+      textInput(inputId = "recall",
+                label = "Recall (% of referrals that are predicted as positive):",
+                value = .97),
       
       # Input: Slider for the annual cost of each reviewer ----
       sliderInput(inputId = "manualReviewCostPerYear",
@@ -48,51 +61,100 @@ ui <- fluidPage(
                  \nTotal cost = (Cost per minute x cost of each missed referral) + (Cost per minute x cost of each manual review)")
     ),
     mainPanel(
-      plotOutput(outputId = "distPlot"),
+      #plotOutput(outputId = "distPlot"),
       DT::dataTableOutput(outputId = "comparisonTable")
     )
   )
 )
 
 server <- function(input, output) {
+
   
+  
+    
   plotData <- reactive({
+    
+    commentCount <- as.numeric(input$commentCount)
+    issueRate <- as.numeric(input$issueRate)
+    issue_count <- commentCount * issueRate
+    normal_count <- commentCount - issue_count
+    precision <- as.numeric(input$precision) # % of predicted positives that are true
+    recall <- as.numeric(input$recall) # % of issue_count that are predicted
+    true_positives <- issue_count * recall
+    predicted_positives <- true_positives / precision
+    false_positives <- predicted_positives - true_positives
+    predicted_negatives <- commentCount - predicted_positives
+    false_negatives <- commentCount - normal_count - true_positives
+    true_negatives <- commentCount - issue_count - false_positives
+    
+    
+    
+    
+    
+    total_texts <- as.numeric(input$numberOfTexts)
+    positive_texts <- as.numeric(input$numberOfPositives)
+    negative_texts <- total_texts - positive_texts
     workdays_each_year <- as.numeric(input$workdaysEachYear)  # 220 average
     manual_review_cost_per_year <- as.numeric(input$manualReviewCostPerYear)                                           # annual salary/benefits etc
     manual_review_cost_per_day <- manual_review_cost_per_year / workdays_each_year    # daily salary
     manual_review_cost_per_minute <- manual_review_cost_per_day / (8 * 60)            # salary by minute
     minutes_per_review <- as.numeric(input$minutesPerManualReview)                                                           # minutes each review takes
     cost_per_false_positive <- minutes_per_review * manual_review_cost_per_minute
+
+    df <- data.frame(
+      commentCount = commentCount,
+      issueRate = issueRate,
+      issue_count = issue_count,
+      normal_count = normal_count,
+      precision = precision,
+      recall = recall,
+      true_positives = true_positives,
+      predicted_positives = round(predicted_positives,0),
+      false_positives = round(false_positives,0),
+      predicted_negatives = round(predicted_negatives,0),
+      false_negatives = round(false_negatives,0),
+      true_negatives = round(true_negatives,0)
+    ) %>% 
+      pivot_longer(everything(), names_to = "Variable")
     
-    df %>% 
-      mutate(total_positives = true_positives + false_negatives,
-             total_negatives = false_positives + true_negatives,
-             total_reviews = total_positives + total_negatives,
-             cost_of_false_positives = cost_per_false_positive * false_positives,
-             cost_of_false_negatives = as.numeric(input$costPerFalseNegative) * false_negatives,
-             total_cost = cost_of_false_positives + cost_of_false_negatives) %>% 
-      mutate_if(is.numeric, as.integer, 0) %>% 
-      mutate(cost_of_false_positives = dollar(cost_of_false_positives),
-             cost_of_false_negatives = dollar(cost_of_false_negatives),
-             `Total Cost` = dollar(total_cost)) %>% 
-      select(process_name,`Total Cost`, everything()) 
+    
+    
+    
+    # df %>% 
+    #   mutate(true_positives = positive_texts * sensitivity,
+    #          false_negatives = positive_texts - true_positives,
+    #          false_positives = negative_texts - (negative_texts * specificity),
+    #          true_negatives = negative_texts * specificity,
+    #          total_positives = true_positives + false_negatives,
+    #          total_negatives = false_positives + true_negatives,
+    #          total_reviews = total_positives + total_negatives,
+    #          cost_of_false_positives = cost_per_false_positive * false_positives,
+    #          cost_of_false_negatives = as.numeric(input$costPerFalseNegative) * false_negatives,
+    #          total_cost = cost_of_false_positives + cost_of_false_negatives) %>% 
+    #   #mutate_if(is.numeric, as.integer, 0) %>% 
+    #   mutate_at(vars(-process_name, -specificity, -sensitivity), as.integer, 0) %>% 
+    #   mutate(cost_of_false_positives = dollar(cost_of_false_positives),
+    #          cost_of_false_negatives = dollar(cost_of_false_negatives),
+    #          `Total Cost` = dollar(total_cost)) %>% 
+    #   select(process_name,`Total Cost`, everything()) 
   })
   
-  output$distPlot <- renderPlot({
-    plotData() %>%
-      ggplot(aes(process_name, total_cost)) +
-      geom_col() + 
-      theme(text = element_text(size=15)) +
-            #axis.text.x = element_text(angle=90, hjust=1)) +
-      coord_flip() +
-      scale_y_continuous(labels=dollar_format(prefix="$")) +
-      labs(x = "Process Name", y = "Total cost")
-  })
+  # output$distPlot <- renderPlot({
+  #   plotData() %>%
+  #     ggplot(aes(process_name, total_cost)) +
+  #     geom_col() + 
+  #     theme(text = element_text(size=15)) +
+  #           #axis.text.x = element_text(angle=90, hjust=1)) +
+  #     coord_flip() +
+  #     scale_y_continuous(labels=dollar_format(prefix="$")) +
+  #     labs(x = "Process Name", y = "Total cost")
+  # })
   
   output$comparisonTable <- DT::renderDataTable({ 
-    DT::datatable(plotData() %>% select(-total_cost), 
+    DT::datatable(plotData(),# %>% select(-total_cost), 
                   rownames= FALSE, 
-                  options = list(bFilter = 0,     #remove filter
+                  options = list(pageLength = 15, #rows to show
+                                 bFilter = 0,     #remove filter
                                  bLengthChange=0))#remove number of items to display dropdown
   })
   
